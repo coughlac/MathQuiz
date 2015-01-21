3e54f820bc0f570f37cb97d5909c5e9b1e2b8c19
coughlac <coughlac@hotmail.com>
2015-01-21 22:56:05 +0000

    CU+BU refactor to inline simple methods and pull out constants

--- src/main/scala/MathQuiz.scala
+++ src/main/scala/MathQuiz.scala
@@ -1,20 +1,15 @@
+import _root_.MathQuiz.{divide, multiply}
+
 import scala.util.Random
 
 class MathQuiz {
 
-  def toQuestion(partsOfSum: IndexedSeq[String] = IndexedSeq("9", "5", "45"), operator: String = "x") :String = {
+  def toQuestion(partsOfSum: IndexedSeq[String], operator: String) :String = {
+    val missingPart : String = "?"
     operator match {
-      case "x" => toMultiplicationQuestion(partsOfSum)
-      case "/" => toDivisionQuestion(partsOfSum)
+      case multiply => formatQuestion(partsOfSum.updated(Random.nextInt(3), missingPart), multiply)
+      case divide => formatQuestion(partsOfSum.updated(Random.nextInt(2), missingPart).reverse, divide)
     }
-  }
-
-  private def toDivisionQuestion(partsOfSum: IndexedSeq[String]): String = {
-    formatQuestion(partsOfSum.updated(Random.nextInt(2), "?").reverse, "/")
-  }
-
-  private def toMultiplicationQuestion(partsOfSum: IndexedSeq[String]): String = {
-    formatQuestion(partsOfSum.updated(Random.nextInt(3), "?"), "x")
   }
 
   private def formatQuestion(question: IndexedSeq[String], operand: String) = {
@@ -23,6 +18,8 @@ class MathQuiz {
 }
 
 object MathQuiz extends App {
+  val multiply: String = "x"
+  val divide : String = "/"
 
   override def main(args: Array[String]) {
     val timesTables = for {
@@ -30,7 +27,7 @@ object MathQuiz extends App {
       j <- 1 until 12
     } yield IndexedSeq(Integer.toString(i), Integer.toString(j), Integer.toString(i * j))
 
-    val operators = IndexedSeq("x","/")
+    val operators = IndexedSeq(multiply,divide)
     Random.shuffle(timesTables).take(60).zipWithIndex.foreach(timesTable => println(s"${1+timesTable._2}. ${new MathQuiz().toQuestion(timesTable._1, Random.shuffle(operators).head)}"))
   }
 }
